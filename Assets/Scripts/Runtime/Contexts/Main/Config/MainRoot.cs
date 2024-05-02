d40f57988c760928e35034d39cf0407e93f80a6f
TayfurSafakGencay <safak.gencay@gmail.com>
2024-05-02 21:05:34 +0300

    New: RootManager

--- Assets/Scripts/Runtime/Contexts/Main/Config/MainRoot.cs
+++ Assets/Scripts/Runtime/Contexts/Main/Config/MainRoot.cs
@@ -4,11 +4,9 @@ namespace Runtime.Contexts.Main.Config
 {
     public class MainRoot : ContextView
     {
-        void Awake()
+        private void Awake()
         {
-            //Instantiate the context, passing it this instance.
             context = new MainContext(this);
-
         }
     }
 }
